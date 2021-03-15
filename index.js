a7d7553ec96b429177b1aed7e7c3138235c4ba0d
pedrosalucas <54561127+pedrosalucas@users.noreply.github.com>
2021-03-14 22:59:14 -0300

    Add Telegram Bot :heavy_plus_sign:

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
+// Discord Bot
 const fs = require('fs');
 require('dotenv').config();
 const Discord = require('discord.js');
-const bot = new Discord.Client();
-bot.commands = new Discord.Collection();
+const botDiscord = new Discord.Client();
+botDiscord.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	bot.commands.set(command.name, command);
+	botDiscord.commands.set(command.name, command);
 }
 
 const PREFIX = process.env.PREFIX;
-const TOKEN = process.env.TOKEN;
+const TOKEN_DISCORD = process.env.TOKEN_DISCORD;
 
-bot.login(TOKEN);
+botDiscord.login(TOKEN_DISCORD);
 
-bot.on('ready', () => {
-	console.info(`Logged in as ${bot.user.tag}!`);
+botDiscord.on('ready', () => {
+	console.info(`Logged in as ${botDiscord.user.tag}!`);
 });
 
-bot.on('message', (message) => {
+botDiscord.on('message', (message) => {
 	if (message.author.bot) return;
 
 	if (message.content.startsWith(PREFIX)) {
@@ -28,7 +29,7 @@ bot.on('message', (message) => {
 		const command = args.shift().toLowerCase();
 
 		try {
-			bot.commands.get(command).execute(message);
+			botDiscord.commands.get(command).execute(message);
 		} catch (error) {
 			console.error(error);
 			message.reply('there was an error trying to execute that command!');
@@ -36,4 +37,25 @@ bot.on('message', (message) => {
 	} else if (message.channel.name === 'telegram-group') {
 		message.channel.send(message.content);
 	}
+});
+
+
+// Telegram Bot
+const TelegramBot = require('node-telegram-bot-api');
+
+const TOKEN_TELEGRAM = process.env.TOKEN_TELEGRAM;
+const botTelegram = new TelegramBot(TOKEN_TELEGRAM, { polling: true });
+
+botTelegram.onText(/%help/, (msg) => {
+	const chatId = msg.chat.id;
+	const resp = 'Essa vai ser uma mensagem de ajuda. : )';
+
+	botTelegram.sendMessage(chatId, resp);
+});
+
+botTelegram.on('message', (msg) => {
+	const chatId = msg.chat.id;
+
+	botTelegram.sendMessage(chatId, `You sent "${msg}".`);
+
 });
